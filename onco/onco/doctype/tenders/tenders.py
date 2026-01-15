d1fa0083dd779eb4d764f1e9e19f5cf7e7250958
mostafamohamed1984 <mustafanazieer@gmail.com>
2026-01-15 20:08:03 +0000

    first commit after removing mena michel from owning this repo

---
+++ onco/onco/doctype/tenders/tenders.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, ds and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Tenders(Document):
+	pass
---
+++ onco/onco/doctype/tenders/test_tenders.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, ds and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTenders(FrappeTestCase):
+	pass
