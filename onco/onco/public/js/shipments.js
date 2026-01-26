c6f3665637185ba2f88f31cc6726e6289b3628e8
mostafamohamed1984 <mustafanazieer@gmail.com>
2026-01-26 21:09:03 +0200

    changes to the shipments doctype2

--- onco/onco/public/js/shipments.js
+++ onco/onco/public/js/shipments.js
@@ -17,19 +17,23 @@ frappe.ui.form.on('Shipment Invoice', {
                 callback: function (r) {
                     if (r.message) {
                         var invoice = r.message;
+                        console.log("Fetched Invoice:", invoice);
+
                         frappe.model.set_value(cdt, cdn, "invoice_no", invoice.bill_no || invoice.name);
                         frappe.model.set_value(cdt, cdn, "invoice_date", invoice.posting_date);
 
                         // If there are items, fetch the first one (or summarize)
                         if (invoice.items && invoice.items.length > 0) {
                             var item = invoice.items[0];
+                            console.log("Fetched First Item:", item);
+
                             frappe.model.set_value(cdt, cdn, "item_code", item.item_code);
                             frappe.model.set_value(cdt, cdn, "item_name", item.item_name);
                             frappe.model.set_value(cdt, cdn, "qty", item.qty);
                             frappe.model.set_value(cdt, cdn, "uom", item.uom);
                             frappe.model.set_value(cdt, cdn, "rate", item.rate);
                             frappe.model.set_value(cdt, cdn, "amount", item.amount);
-                            frappe.model.set_value(cdt, cdn, "batch_no", item.batch_no);
+                            frappe.model.set_value(cdt, cdn, "batch_no", item.batch_no || "");
                         }
                     }
                 }
